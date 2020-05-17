2049e2b95a37c9284044f918eb5c2c33de81a286
benjamin <bappiahpoku@gmail.com>
2020-05-17 12:42:46 +0000

    update commit 6

--- git.sh
+++ git.sh
@@ -11,9 +11,9 @@ sleep 1
 if git push origin master
 then
     echo "Files uploaded to Github"
-    sleep 1 
-    git pull origin master
-    echo "Files synced with Github"
+    # sleep 1 
+    # git pull origin master
+    # echo "Files synced with Github"
 else
     echo "Unable to upload files to Github"
 fi
